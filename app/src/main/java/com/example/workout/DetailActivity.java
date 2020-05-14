f0898bf5e980a690f89d618662e71712458138f5
Ilya-Grigorev <zzzzzzzzzzzz@mail.ru>
2020-05-14 15:38:06 +0300

    commit 1

---
+++ app/src/main/java/com/example/workout/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.workout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// github:
+// Android_HeadFirst_Chapter9_Workout
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
